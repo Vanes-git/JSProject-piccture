5047341604c01f4cab33f8538d2511ef72d65d8c
Ioann <vanes.sysoev@gmail.com>
2021-05-28 16:54:20 +0300

    feat: giftModal
    scroll popup and other optimizations

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,9 +1,10 @@
 const modals = () => {
+    let btnPressed = false;
     function bindModal(
         triggerSelector,
         modalSelector,
         closeSelector,
-        closeClickOverlay = true
+        destroy = false
     ) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
@@ -17,8 +18,15 @@ const modals = () => {
                     e.preventDefault();
                 }
 
+                btnPressed = true;
+
+                if (destroy) {
+                    item.remove();
+                }
+
                 windows.forEach((item) => {
                     item.style.display = "none";
+                    item.classList.add('animated', 'fadeIn');
                 });
 
                 modal.style.display = "block";
@@ -38,7 +46,7 @@ const modals = () => {
         });
 
         modal.addEventListener("click", (e) => {
-            if (e.target === modal && closeClickOverlay) {
+            if (e.target === modal) {
                 windows.forEach((item) => {
                     item.style.display = "none";
                 });
@@ -63,6 +71,8 @@ const modals = () => {
             if (!display) {
                 document.querySelector(selector).style.display = "block";
                 document.body.style.overflow = "hidden";
+                let scroll = calcScroll();
+                document.body.style.marginRight = `${scroll}px`;
             }
         }, time);
     }
@@ -82,12 +92,26 @@ const modals = () => {
         return scrollWidth;
     }
 
+    function openByScroll(selector) {
+        window.addEventListener("scroll", () => {
+            if (
+                !btnPressed &&
+                (window.pageYOffset + document.documentElement.clientHeight >=
+                    document.documentElement.scrollHeight)
+            ) {
+                document.querySelector(selector).click();
+            }
+        });
+    }
+
     bindModal(".button-design", ".popup-design", ".popup-design .popup-close");
     bindModal(
         ".button-consultation",
         ".popup-consultation",
         ".popup-consultation .popup-close"
     );
+    bindModal(".fixed-gift", ".popup-gift", ".popup-gift .popup-close", true);
+    openByScroll('.fixed-gift');
 
     showModalByTime(".popup-consultation", 5000);
 };
